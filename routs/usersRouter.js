d20843bd2844a2be2eaac1261d71fb6de5caf3c3
Amittes <amittes@gmail.com>
2021-01-05 22:49:33 +0200

    fix modal close when not pressing button

--- routs/usersRouter.js
+++ routs/usersRouter.js
@@ -8,6 +8,7 @@ const { get } = require('./petsRouter');
 function authenticateToken(req, res, next) {
 	const authHeader = req.headers['authorization'];
 	const token = authHeader && authHeader.split(' ')[1];
+	console.log(token);
 	if (token == null) return res.sendStatus(401);
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
 		if (err) return res.sendStatus(403);

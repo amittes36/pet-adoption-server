8d0572fbd0195b9440a698d0ed836cda82ff9729
Amittes <amittes@gmail.com>
2021-01-11 20:10:36 +0200

    4

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 mongoose.connect(
-	`mongodb+srv://admin_user:${process.env.DB_PASS}@cluster0.8pu0h.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+	process.env.MONGODB_URI ||
+		`mongodb+srv://admin_user:${process.env.DB_PASS}@cluster0.8pu0h.mongodb.net/<dbname>?retryWrites=true&w=majority`,
 	{
 		useUnifiedTopology: true,
 		useNewUrlParser: true,
@@ -21,6 +21,10 @@ app.use(
 		parameterLimit: 50000,
 	})
 );
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('pet-adoption-front-end-amittes36/build'));
+}
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
@@ -28,4 +32,6 @@ const usersRouter = require('./routs/usersRouter.js');
 app.use('/usersTest', usersRouter);
 const petsRouter = require('./routs/petsRouter.js');
 app.use('/pets', petsRouter);
-app.listen(port, () => {});
+app.listen(PORT, () => {
+	console.log(`server is running on port ${PORT}`);
+});
